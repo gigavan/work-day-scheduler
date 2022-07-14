7a381c7bc78d1c3b76a28b5a39fe3f4aa2d6fc6a
Emmanuel <Gigavan@gmail.com>
2022-07-14 17:01:42 -0400

    figured out time and initial column and div

--- assets/Java/jscripts.js
+++ assets/Java/jscripts.js
@@ -1,9 +1,17 @@
-//add time blocks from 9am to 5pm
+$(document).ready(function () {// tells engine to load 1)html & 2)css
+    //display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
+    //assign saveBtn click listener for user input
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
 
-const now = moment();
-//adding current day at the top
-var currentTime = moment().format("MMMM DD YYYY");
-$("#currentDay").text(currentTime);
+        //set items in local storage.
+        localStorage.setItem(time, text);
+    })
+    //load any saved data from LocalStorage - do this for each hour created.
+    $("#h8 .description").val(localStorage.getItem("h8"));
 
-
-//variable for now
+});
