499ab26a3e97529be1e7852be429e9c0420e644a
Emmanuel <Gigavan@gmail.com>
2022-07-14 15:05:06 -0400

    got time added at the top

---
+++ assets/Java/jscripts.js
@@ -0,0 +1,9 @@
+//add time blocks from 9am to 5pm
+
+const now = moment();
+//adding current day at the top
+var currentTime = moment().format("MMMM DD YYYY");
+$("#currentDay").text(currentTime);
+
+
+//variable for now
